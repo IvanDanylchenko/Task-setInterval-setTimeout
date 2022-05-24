08275695001116e8eea9aa795d0f58554af60d0f
IvanDanylchenko <danylchenko21@gmail.com>
2022-05-24 18:20:44 +0300

    Fix bug index.js

--- js/index.js
+++ js/index.js
@@ -2,30 +2,28 @@
 
 // setInterval
 
-function printNumbers (from, to, interval) {
-  let value = from;
-  const methotInterval = setInterval(function () {
-    console.log('value', value);
-    if (value === to) {
-      clearInterval(methotInterval);
-    }
-    value++;
-  }, interval);
-}
-
-printNumbers(-3, 5, 1000);
-
-// setTimeout
-
 // function printNumbers (from, to, interval) {
 //   let value = from;
-//   setTimeout(function recursiveInterval () {
-//     console.log('value', value);
-//     if (value < to) {
-//       setTimeout(recursiveInterval);
+//   const methotInterval = setInterval(function () {
+//     if (value === to) {
+//       clearInterval(methotInterval);
 //     }
 //     value++;
 //   }, interval);
 // }
 
 // printNumbers(-3, 5, 1000);
+
+// setTimeout
+
+function printNumbers (from, to, interval) {
+  let value = from;
+  setTimeout(function recursiveInterval () {
+    if (value < to) {
+      setTimeout(recursiveInterval, 5000);
+    }
+    value++;
+  }, interval);
+}
+
+printNumbers(-3, 5, 1000);
